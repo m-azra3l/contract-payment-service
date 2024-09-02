fa1b13e659401d58153b34db39953071802eed16
Michael <gadreelazazel@gmail.com>
2024-09-02 22:17:19 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Accessing ConfigService to retrieve the port value
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
 
   // Swagger setup
   const config = new DocumentBuilder()
     .setTitle('Contract Payment Service')
     .setDescription('API documentation for the Contract Payment Service')
     .setVersion('1.0')
-    .addBearerAuth()
-    .addServer('http://localhost:3000', 'Development server')
+    .addServer(`http://localhost:${port}`, 'Development server')
     .addServer('https://to-be-specified', 'Production server')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
