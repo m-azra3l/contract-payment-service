5779e5ec1e9c93926cb96e0b374256c02740569b
Michael <gadreelazazel@gmail.com>
2024-09-03 09:33:39 +0100

    Create get-profile.dto.ts

---
+++ src/auth/dto/get-profile.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ProfileRole } from '@prisma/client';
+import { Decimal } from '@prisma/client/runtime/library';
+
+export class GetProfileDto {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: 'ab3cd23e-000a-w752-9df2-61cffa158b23' })
+  uuid: string;
+
+  @ApiProperty({ example: 'James' })
+  firstName: string;
+
+  @ApiProperty({ example: 'Gun' })
+  lastName: string;
+
+  @ApiProperty({ example: 'Consultant' })
+  profession: string;
+
+  @ApiProperty({ example: 1500.0 })
+  balance: Decimal;
+
+  @ApiProperty({ example: 'client' || 'contractor' })
+  role: ProfileRole;
+
+  @ApiProperty({ example: '2024-09-02T22:39:24.093Z' })
+  createdAt: Date;
+
+  @ApiProperty({ example: '2024-09-02T22:39:24.093Z' })
+  updatedAt: Date;
+}
