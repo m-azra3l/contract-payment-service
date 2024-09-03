39c6e7adb21bc67ddd35cc351ae9799e678ff1df
Michael <gadreelazazel@gmail.com>
2024-09-03 09:23:38 +0100

    Update get-contract.dto.ts

--- src/contracts/dto/get-contract.dto.ts
+++ src/contracts/dto/get-contract.dto.ts
@@ -5,15 +5,24 @@ export class GetContractDto {
   @ApiProperty({ example: 1 })
   id: number;
 
+  @ApiProperty({ example: 'ab3cd23e-000a-w752-9df2-61cffa158b23' })
+  uuid: string;
+
   @ApiProperty({ example: 'Contract for web development' })
   terms: string;
 
   @ApiProperty({ enum: ContractStatus, example: 'in_progress' })
   status: ContractStatus;
 
-  @ApiProperty({ example: 1 })
-  contractorId: number;
+  @ApiProperty({ example: 'John Doe' })
+  clientName: string;
 
-  @ApiProperty({ example: 2 })
-  clientId: number;
+  @ApiProperty({ example: 'Jane Smith' })
+  contractorName: string;
+
+  @ApiProperty({ example: '2024-09-02T00:00:00Z' })
+  createdAt: Date;
+
+  @ApiProperty({ example: '2024-09-02T00:00:00Z' })
+  updatedAt: Date;
 }
