faca48ce00b6fe5c9d61f11d80e4be9d40218f1a
Michael <gadreelazazel@gmail.com>
2024-09-03 12:45:23 +0100

    Update admin.controller.ts

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,20 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiProperty,
+} from '@nestjs/swagger';
 import { AdminService } from './admin.service';
+import { BestClientDto } from './dto/best-client.dto';
+
+class BestProfessionDto {
+  @ApiProperty({
+    description: 'The best profession',
+    example: 'Contractor' || null,
+  })
+  profession: string | null;
+}
 
 @ApiTags('Admin')
 @Controller('admin')
@@ -15,11 +29,12 @@ export class AdminController {
   @ApiResponse({
     status: 200,
     description: 'Best profession retrieved successfully',
+    type: BestProfessionDto,
   })
   async getBestProfession(
     @Query('start') start: string,
-    @Query('end') end: string,
-  ): Promise<string> {
+    @Query('end') end: string
+  ): Promise<object> {
     const startDate = new Date(start);
     const endDate = new Date(end);
     return this.adminService.getBestProfession(startDate, endDate);
@@ -32,12 +47,13 @@ export class AdminController {
   @ApiResponse({
     status: 200,
     description: 'Best clients retrieved successfully',
+    type: [BestClientDto],
   })
   async getBestClients(
     @Query('start') start: string,
     @Query('end') end: string,
-    @Query('limit') limit: string = '2',
-  ) {
+    @Query('limit') limit: string = '2'
+  ): Promise<BestClientDto[]> {
     const startDate = new Date(start);
     const endDate = new Date(end);
     return this.adminService.getBestClients(startDate, endDate, Number(limit));
